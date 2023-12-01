9e78b80915a551874a4f3f0ef3a15f3fc6b98692
Mark Johnston <mark@protectchildren.ca>
2023-12-01 12:05:17 -0600

    Initial skeleton

---
+++ src/day.rs
@@ -0,0 +1,11 @@
+use color_eyre::Report;
+use tracing::info;
+
+pub(crate) fn solve(input: String) -> Result<(), Report> {
+
+    info!(day=1, part=1, answer=);
+
+    //info!(day=1, part=2, answer=);
+
+    Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,53 @@
+use std::fs::read_to_string;
+use std::path::PathBuf;
+
+use color_eyre::Report;
+use clap::Parser;
+use tracing::debug;
+use tracing_subscriber::EnvFilter;
+
+#[derive(Parser, Debug)]
+#[command()]
+struct Args {
+    #[arg(short, long)]
+    debug: bool,
+
+    #[arg(short, long)]
+    puzzle: u32,
+
+    #[arg(short, long)]
+    input: PathBuf
+}
+
+fn set_up_logging(debug: bool) -> Result<(), Report> {
+    if std::env::var("RUST_LIB_BACKTRACE").is_err() && debug {
+        std::env::set_var("RUST_LIB_BACKTRACE", "full" );
+    }
+
+    color_eyre::install()?;
+
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", if debug { "debug" } else { "info" });
+    }
+
+    tracing_subscriber::fmt::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .init();
+
+    Ok(())
+}
+
+fn main() -> Result<(), Report> {
+    let args = Args::parse();
+    set_up_logging(args.debug)?;
+
+    let input = read_to_string(&args.input)?;
+
+    debug!("{file:?}: read {count} bytes", file=args.input, count=input.len());
+
+    match args.puzzle {
+        _ => panic!("No such puzzle: {day}", day=args.puzzle)
+    }
+
+    Ok(())
+}
